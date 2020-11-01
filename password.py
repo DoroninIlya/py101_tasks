f2245f44b705fbcc690c098001f845416bee2698
Ilya Doronin <ilyaroadar@gmail.com>
2020-11-01 11:13:21 +0300

    password is done

--- password.py
+++ password.py
@@ -8,3 +8,11 @@
 
 if __name__ == '__main__':
     pass
+
+entered_password = input()
+
+if (len(entered_password) >= 8 and entered_password.isalnum() and not
+        entered_password.isalpha() and not entered_password.isnumeric()):
+    print('сложный')
+else:
+    print('простой')
