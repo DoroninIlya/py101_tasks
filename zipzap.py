cf9407032db84ebd57bc57078ff5088359f19b36
Ilya Doronin <ilyaroadar@gmail.com>
2020-11-01 11:59:27 +0300

    zipzap is done

--- zipzap.py
+++ zipzap.py
@@ -10,3 +10,20 @@
 
 if __name__ == '__main__':
     pass
+
+INITIAL_NUMBER = 1
+FINISH_NUMBER = 101
+
+NUMBER_FOR_ZIP = 3
+NUMBER_FOR_ZAP = 5
+NUMBER_FOR_ZIPZAP = 15
+
+for number in range(INITIAL_NUMBER, FINISH_NUMBER):
+    if number % NUMBER_FOR_ZIP == 0 and number % NUMBER_FOR_ZIPZAP != 0:
+        print('zip')
+    elif number % NUMBER_FOR_ZAP == 0 and number % NUMBER_FOR_ZIPZAP != 0:
+        print('zap')
+    elif number % NUMBER_FOR_ZIPZAP == 0:
+        print('zip-zap')
+    else:
+        print(number)
