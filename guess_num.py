27f3c1049c87e7fcce64bbc6c257c4ca823d1c06
Ilya Doronin <ilyaroadar@gmail.com>
2020-11-01 14:22:07 +0300

    guess_num is done

--- guess_num.py
+++ guess_num.py
@@ -16,3 +16,36 @@
 
 if __name__ == '__main__':
     pass
+
+import random
+
+INITIAL_NUMBER = 1
+FINISH_NUMBER = 1000000
+
+random_number = random.randint(INITIAL_NUMBER, FINISH_NUMBER)
+
+message_with_range = f'от {INITIAL_NUMBER} до {FINISH_NUMBER}'
+
+print(f'''Здравствуйте, пользователь!
+Введите число в диапазоне {message_with_range}:''')
+
+while True:
+    entered_string = input()
+
+    if 'exit' in entered_string.lower() or entered_string == '':
+        break
+    elif not entered_string.isnumeric():
+        print('Вы ввели не число')
+    else:
+        entered_number = int(entered_string)
+
+        if INITIAL_NUMBER <= entered_number <= FINISH_NUMBER:
+            if entered_number < random_number:
+                print('Введенное число меньше загаданного')
+            elif entered_number > random_number:
+                print('Введенное число больше загаданного')
+            else:
+                print('Вы угадали!')
+                break
+        else:
+            print(f'Введенное число вне диапазона {message_with_range}')
